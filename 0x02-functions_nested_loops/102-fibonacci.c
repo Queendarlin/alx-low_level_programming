a903b7404612c0aee2cdca244c108d533bdcef23
Queendarlin <queendarlinnnamani@gmail.com>
2023-09-12 16:34:09 -0700

    A , 63245986, 102334155, 165580141, 267914296, 433494437, 701408733, 1134903170, 1836311903, 2971215073, 4807526976, 7778742049, 12586269025, 20365011074 root@a1e4a4a99470:~/alx-low_level_programming/0x02-functions_nested_loops# a root@a1e4a4a99470:~/alx-low_level_programming/0x02-functions_nested_loops# c A

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Enrty point of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int first_count = 1;
+	unsigned long int second_count = 2;
+	unsigned long int next_count;
+	int c;
+
+	printf("%lu, ", first_count);
+	for (c = 1; c < 50; c++)
+	{
+		printf("%lu", second_count);
+		next_count = first_count + second_count;
+		first_count = second_count;
+		second_count = next_count;
+		if (c != 49)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}

c91329501954bb7d63462e0f9649092c4bdc88da
Queendarlin <queendarlinnnamani@gmail.com>
2023-09-20 04:38:04 -0700

    A program for a function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * *_strncat - Function to concatenate two strings
+ * @dest: The string to be concatenated
+ * @src: The second string to be concatenated
+ * @n: the number of bytes
+ * Reurn: Returns the value of the concatenate
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+}

ea7fbda4298c958bcaa5694df7f1b950da1080ea
Queendarlin <queendarlinnnamani@gmail.com>
2023-09-20 08:02:45 -0700

    A program for a function that copies a string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,9 +5,19 @@
  * @dest: The string to be concatenated
  * @src: The second string to be concatenated
  * @n: the number of bytes
- * Reurn: Returns the value of the concatenate
+ * Return: Returns the value of the concatenate
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	int length_dest = 0;
+	int index;
 
+	for (index = 0; dest[length_dest] != '\0'; index++)
+		length_dest++;
+	for (index = 0; index < n && src[index] != '\0'; index++)
+	{
+		dest[length_dest + index] = src[index];
+	}
+	dest[length_dest + index] = '\0';
+	return (dest);
 }

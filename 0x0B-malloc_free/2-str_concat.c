6f0f86e346cc70332dbcc172d4b48ff08776ab55
Queendarlin <queendarlinnnamani@gmail.com>
2023-10-03 15:33:14 -0700

    A program for a function that concatenates two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * str_concat - Function to concatenate two string
+ *@s1: First string to concatenate
+ *@s2: Second string to concatenate
+ *Return: pointer to the content of s1 and s2 or NULL
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int s1length = 0;
+	int s2length = 0;
+	char *conc;
+	int index;
+
+	if (s1 == NULL)
+		return (NULL);
+	if (s2 == NULL)
+		return (NULL);
+	for (index = 0; s1[index] != '\0'; index++)
+		s1length++;
+	for (index = 0; s2[index] != '\0'; index++)
+		s2length++;
+	conc = malloc(sizeof(char) * (s1length + s2length) + 1);
+	if (conc == NULL)
+		return (NULL);
+	for (index = 0; s1[index] != '\0'; index++)
+		conc[index] = s1[index];
+	for (index = 0; s2[index] != '\0'; index++)
+		conc[s1length + index] = s2[index];
+	return (conc);
+}

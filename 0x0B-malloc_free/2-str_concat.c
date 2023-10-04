abb4938ba2a4c60407728e9d434f8ce71a373aac
Queendarlin <queendarlinnnamani@gmail.com>
2023-10-03 22:54:52 -0700

    A program for a function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,9 @@ char *str_concat(char *s1, char *s2)
 	int index;
 
 	if (s1 == NULL)
-		return ("");
+		s1 = "";
 	if (s2 == NULL)
-		return ("");
+		s2 = "";
 	for (index = 0; s1[index] != '\0'; index++)
 		s1length++;
 	for (index = 0; s2[index] != '\0'; index++)

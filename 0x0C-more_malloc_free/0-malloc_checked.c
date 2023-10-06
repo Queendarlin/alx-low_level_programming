94610d211f95f72c8656ea5e9c7fe0883781d29a
Queendarlin <queendarlinnnamani@gmail.com>
2023-10-06 08:23:55 -0700

    A proram for a function that concatenates two strings

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Function to allocate memory using malloc
+ * @b: Parameter to determine the size of memory to allocate
+ * Return: A pointer to the allocated memory or NULL
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *space;
+
+	space = malloc(b);
+	if (space == NULL)
+		exit(98);
+	else
+		return (space);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * string_nconcat - Function to concatenate two strings and allocate memory
+ * @s1: First string to concatenate
+ * @s2: Second string to concatenate
+ * Return: A pointer to the concatenated string or NULL
+ * @n: Number of bytes to concatenate from s2
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int s1length = 0;
+	unsigned int s2length = 0;
+	char *conc;
+	unsigned int index;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (index = 0; s1[index] != '\0'; index++)
+		s1length++;
+	for (index = 0; s2[index] != '\0'; index++)
+		s2length++;
+	conc = malloc(sizeof(char) * (s1length + n) + 1);
+	if (conc == NULL)
+		return (NULL);
+	if (n >= s2length)
+	{
+		for (index = 0; index < s1length; index++)
+			conc[index] = s1[index];
+		for (index = 0; index < s2length; index++)
+			conc[s2length + index] = s2[index];
+		conc[s2length + index] = '\0';
+	}
+	else
+	{
+		for (index = 0; index < s1length; index++)
+			conc[index] = s1[index];
+		for (index = 0; index < n; index++)
+			conc[s1length + index] = s2[index];
+		conc[s1length + index] = '\0';
+	}
+	return (conc);
+}

7b752aa5a2698d68c61c7df96cf217e161b6ee7a
Queendarlin <queendarlinnnamani@gmail.com>
2023-10-07 03:59:52 -0700

    A program for a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,13 +1,23 @@
 #include "main.h"
 
 /**
- * _positive_int - Function to check if a string consists of only digits
- * @string: The string to check
- * Return: 1 if it's a positive integer, 0 otherwise
+ * error_exit - Function to print error message and exit with status 98
  */
-int _positive_int(char *string)
+void error_exit(void)
 {
-	if (string == NULL || *string == '\0')
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * is_positive_number - Function to check if a string is a positive number
+ * @string: The string to check.
+ *
+ * Return: 1 if it's a positive number, 0 otherwise
+ */
+int is_positive_number(char *string)
+{
+	if (*string == '\0')
 		return (0);
 
 	while (*string)
@@ -16,31 +26,30 @@ int _positive_int(char *string)
 			return (0);
 		string++;
 	}
-
 	return (1);
 }
 
 /**
- * main - Function to multiply two positive numbers
- * @argc: The number of command-line arguments
- * @argv: An array of command-line argument strings
- * Return: 0 on success, or 98 on error.
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array containing the command-line arguments.
+ *
+ * Return: 0 if successful, otherwise 98 for errors.
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
+	int num1, num2;
 
-	if (argc != 3 || !_positive_int(argv[1]) || !_positive_int(argv[2]))
-	{
-		printf("Error\n");
-		return (98);
-	}
+	if (argc != 3)
+		error_exit();
+
+	if (!is_positive_number(argv[1]) || !is_positive_number(argv[2]))
+		error_exit();
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
-	result = num1 * num2;
 
-	printf("%d\n", result);
+	printf("%d\n", num1 * num2);
 
 	return (0);
 }

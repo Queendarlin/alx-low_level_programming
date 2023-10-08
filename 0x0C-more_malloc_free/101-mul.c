d4f87912ad62343b888867d2c189be0249631d4b
Queendarlin <queendarlinnnamani@gmail.com>
2023-10-08 09:47:11 -0700

    A program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,55 +1,97 @@
 #include "main.h"
 
+#define ERR_MSG "Error"
+
 /**
- * error_exit - Function to print error message and exit with status 98
+ * is_digit - Function to check if a string has only digit characters
+ * @string: The string to be checked
+ * Return: 1 if the string contains only digits, else 0
  */
-void error_exit(void)
+int is_digit(char *string)
+{
+	int index = 0;
+
+	while (string[index])
+	{
+		if (string[index] < '0' || string[index] > '9')
+			return (0);
+		index++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - Function to compute the length of a string
+ * @string: The string to compute
+ *
+ * Return: Length of string
+ */
+int _strlen(char *string)
+{
+	int index = 0;
+
+	while (string[index] != '\0')
+	{
+		index++;
+	}
+	return (index);
+}
+
+/**
+ * errors - Function to handle errors for the main function
+ */
+void errors(void)
 {
 	printf("Error\n");
 	exit(98);
 }
 
 /**
- * is_positive_number - Function to check if a string is a positive number
- * @string: The string to check.
- *
- * Return: 1 if it's a positive number, 0 otherwise
- */
-int is_positive_number(char *string)
-{
-	if (*string == '\0')
-		return (0);
-
-	while (*string)
-	{
-		if (*string < '0' || *string > '9')
-			return (0);
-		string++;
-	}
-	return (1);
-}
-
-/**
- * main - Entry point of the program.
- * @argc: The number of command-line arguments.
- * @argv: An array containing the command-line arguments.
- *
- * Return: 0 if successful, otherwise 98 for errors.
+ * main - Function to Multiply two positive numbers
+ * @argc: The number of command-line arguments
+ * @argv: The Array of command-line arguments
+ * Return: Always returns 0 (Success).
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
+	char *string1, *string2;
+	int len1, len2, result_len, index, hold, num1, num2, *result, zero = 0;
 
-	if (argc != 3)
-		error_exit();
-
-	if (!is_positive_number(argv[1]) || !is_positive_number(argv[2]))
-		error_exit();
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-
-	printf("%d\n", num1 * num2);
-
+	string1 = argv[1], string2 = argv[2];
+	if (argc != 3 || !is_digit(string1) || !is_digit(string2))
+		errors();
+	len1 = _strlen(string1);
+	len2 = _strlen(string2);
+	result_len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * result_len);
+	if (!result)
+		return (1);
+	for (index = 0; index <= len1 + len2; index++)
+		result[index] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		num1 = string1[len1] - '0';
+		hold = 0;
+		for (len2 = _strlen(string2) - 1; len2 >= 0; len2--)
+		{
+			num2 = string2[len2] - '0';
+			hold += result[len1 + len2 + 1] + (num1 * num2);
+			result[len1 + len2 + 1] = hold % 10;
+			hold /= 10;
+		}
+		if (hold > 0)
+			result[len1 + len2 + 1] += hold;
+	}
+	for (index = 0; index < result_len - 1; index++)
+	{
+		if (result[index])
+			zero = 1;
+		if (zero)
+			_putchar(result[index] + '0');
+	}
+	if (!zero)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }

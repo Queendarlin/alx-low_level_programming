b7e7f8e4b5ea08f4f0eff05a91510f8e04826ef1
Queendarlin <queendarlinnnamani@gmail.com>
2023-10-10 04:47:40 -0700

    A structure for a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A structure with the name dog
+ * @name: The member for the name of the dog
+ * @age: The member for the age of the dog
+ * @owner: The member for the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

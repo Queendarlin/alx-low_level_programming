fd0f45fa9065c6d0de4b3f95fce25c07ab1bbebe
Queendarlin <queendarlinnnamani@gmail.com>
2023-10-10 14:37:21 -0700

    A program for a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,54 +1,86 @@
 #include "dog.h"
-#include <string.h>
 
 /**
- * new_dog - Function to create a new dog and returns a pointer to it
+ * _strlen - Function to calculate the length of a string
+ * @string: The input string to check
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *string)
+{
+	int length;
+
+	length = 0;
+
+	while (string[length] != '\0')
+	{
+		length++;
+	}
+
+	return (length);
+}
+
+/**
+ * _strcpy - Function to copy the source string to the destination buffer
+ * @dest: A pointer to the destination buffer
+ * @src: The source string to be copied
+ *
+ * Return: A pointer to the destination buffer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int length = 0, index;
+
+	while (src[length] != '\0')
+	{
+		length++;
+	}
+
+	for (index = 0; index < length; index++)
+	{
+		dest[index] = src[index];
+	}
+	dest[index] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - Function to create a new dog
  * @name: The name of the new dog
  * @age: The age of the new dog
  * @owner: The owner of the new dog
  *
- * Return: A pointer to the newly created dog_t structure
+ * Return: A pointer to the newly created dog structure, else NULL
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *newDog = malloc(sizeof(dog_t));
+	dog_t *dog;
+	int len_name, len_owner;
 
-	if (newDog == NULL)
+	len_name = _strlen(name);
+	len_owner = _strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (len_name + 1));
+	if (dog->name == NULL)
 	{
+		free(dog);
 		return (NULL);
 	}
-
-	newDog->name = strdup(name);
-
-	if (newDog->name == NULL)
+	dog->owner = malloc(sizeof(char) * (len_owner + 1));
+	if (dog->owner == NULL)
 	{
-		free(newDog);
+		free(dog);
+		free(dog->name);
 		return (NULL);
 	}
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
 
-	newDog->age = age;
-	newDog->owner = strdup(owner);
-
-	if (newDog->owner == NULL)
-	{
-		free(newDog->name);
-		free(newDog);
-		return (NULL);
-	}
-
-	return (newDog);
-}
-
-/**
- * free_dog - Frees memory allocated for a dog.
- * @d: A pointer to the dog_t structure to be freed.
- */
-void free_dog(dog_t *d)
-{
-	if (d != NULL)
-	{
-		free(d->name);
-		free(d->owner);
-		free(d);
-	}
+	return (dog);
 }
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,11 +7,10 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d != NULL)
+	if (d)
 	{
-		if (d->name != NULL)
-			free(d->name);
-		if (d->owner != NULL)
-			free(d->owner);
+		free(d->name);
+		free(d->owner);
+		free(d);
 	}
 }

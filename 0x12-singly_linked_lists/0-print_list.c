5abd595b622d61639896e0b83d9cd7f86b4b5feb
Queendarlin <queendarlinnnamani@gmail.com>
2023-10-19 11:51:13 -0700

    A program for a function that prints all the elements of a list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,12 +9,12 @@ size_t print_list(const list_t *h)
 {
 	size_t num_nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 
 		h = h->next;
 		num_nodes++;

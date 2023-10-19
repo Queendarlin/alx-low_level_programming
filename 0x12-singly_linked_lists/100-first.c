d62e45aa55bfcac767621fa7c5c6ef80e4db8650
Queendarlin <queendarlinnnamani@gmail.com>
2023-10-19 12:23:03 -0700

    A program for a function that prints strings before main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * before_exe - Function to print strings before the program's execution
+ */
+void before_exe(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

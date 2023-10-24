2dc4a2fd4ab7b7c936891af781a6a2c1fd4c8081
Queendarlin <queendarlinnnamani@gmail.com>
2023-10-24 08:17:56 -0700

    A program for a function that finds the loop in a linked list

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -6,3 +6,31 @@
  * Return: Address of the node where looping starts on success or NULL
  */
 listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *one_node, *two_nodes;
+
+	if (head == NULL)
+		return (NULL);
+
+	one_node = head;
+	two_nodes = head;
+
+	while (two_nodes && two_nodes->next)
+	{
+		one_node = one_node->next;
+		two_nodes = two_nodes->next->next;
+
+		if (one_node == two_nodes)
+		{
+			one_node = head;
+			while (one_node != two_nodes)
+			{
+				one_node = one_node->next;
+				two_nodes = two_nodes->next;
+			}
+			return (one_node);
+		}
+	}
+
+	return (NULL);
+}

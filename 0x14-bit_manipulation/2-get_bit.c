8f84a825e0219f9d3af454ae9de1092eae3d7add
Queendarlin <queendarlinnnamani@gmail.com>
2023-10-26 10:05:37 -0700

    A program for a function that returns the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,4 +8,13 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned 
+	unsigned long int mask;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	mask = 1UL << index;
+	if ((n & mask) == mask)
+		return (1);
+	else
+		return (0);
+}

d5a9d2d2194866f8f99cddd19041e9cf868d403d
Queendarlin <queendarlinnnamani@gmail.com>
2023-10-26 10:35:17 -0700

    A program for a function that sets the value of a bit to 1 at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,12 +8,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int mask;
+	unsigned long int mask_bit;
 
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	mask = 1UL << index;
-	if ((n & mask) == mask)
+	mask_bit = 1UL << index;
+	if ((n & mask_bit) == mask_bit)
 		return (1);
 	else
 		return (0);
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,9 +1,19 @@
 #include "main.h"
 
 /**
- * set_bit: Function to set the value of a bit to 1 at a given index
+ * set_bit - Function to set the value of a bit to 1 at a given index
  * @n: The number to set to 1
  * @index: The index to evaluate
  * Return: 1 if it worked, or -1 if an error occurred
  */
 int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask_set;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	mask_set = 1UL << index;
+	*n = (*n | mask_set);
+	return (1);
+}

20a74a6dadb7b21d445b1abfde974492e9130f31
Queendarlin <queendarlinnnamani@gmail.com>
2023-10-26 11:18:24 -0700

    A program for a function that checks the endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -5,3 +5,12 @@
  * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
+{
+	unsigned int byte_num = 1;
+	char *byte_ptr = (char *)&byte_num;
+
+	if (*byte_ptr == 1)
+		return (1);
+	else
+		return (0);
+}

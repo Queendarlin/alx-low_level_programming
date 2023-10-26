161c467c5aa022bf3c691a7ecd4d6e78016037d7
Queendarlin <queendarlinnnamani@gmail.com>
2023-10-26 09:54:05 -0700

    A program for a function that prints the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,3 +6,24 @@
  * Return: The printed binary
  */
 void print_binary(unsigned long int n)
+{
+	unsigned int bit_set = 0;
+	unsigned int index;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	for (index = sizeof(unsigned long int) * 8 - 1;
+			index < sizeof(unsigned long int) * 8; index--)
+	{
+		if (n & (1UL << index))
+		{
+			bit_set = 1;
+			_putchar('1');
+		}
+		else if (bit_set)
+			_putchar('0');
+	}
+}
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,3 +7,5 @@
  * Return: The value of the bit at index index or -1 if an error occured
  */
 int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned 

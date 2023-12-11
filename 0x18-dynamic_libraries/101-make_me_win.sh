ea729e7d7474f23033f99856094bba099d13f12c
Queendarlin <queendarlinnnamani@gmail.com>
2023-12-11 08:25:24 -0800

    Hacking is not easy

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Queendarlin/alx-low_level_programming/master/0x18-dynamic_libraries/libhacktest.so
-export Ld_PRELOAD="$PWD/../libhacktest.so"
+wget -P .. https://raw.githubusercontent.com/Queendarlin/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export Ld_PRELOAD="$PWD/../libgiga.so"

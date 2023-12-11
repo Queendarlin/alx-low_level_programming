cd96dba82a59db7c9d07852d8c4c6a31921de2a5
Queendarlin <queendarlinnnamani@gmail.com>
2023-12-11 06:39:31 -0800

    mss

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-(echo -e '#include <stdlib.h>\n#include <time.h>\n\nint rand(void) { srand(time(NULL)); return 9; }' > /tmp/hack_rand.c && gcc -shared -fPIC /tmp/hack_rand.c -o /tmp/hack_rand.so && export LD_PRELOAD=/tmp/hack_rand.so)
+gcc -shared -o myprintf.so -fPIC test.c
+export LD_PRELOAD=./myprintf.so:$LD_LIBRARY_PATH

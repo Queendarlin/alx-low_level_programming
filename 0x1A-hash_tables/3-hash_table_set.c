0a62e86f70555d4ad827d1647549538515e4368d
Queendarlin <queendarlinnnamani@gmail.com>
2023-12-21 12:12:18 -0800

    A function that adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node, *temp_ptr;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (ht == NULL || strcmp(key, "") == 0 || ht == NULL)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
